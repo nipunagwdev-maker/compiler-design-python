596f5812b027f0b80e8587b15dd0d5ac0019cb32
nipunagwdev-maker <nipuna.gw.dev@gmail.com>
2026-09-02 09:20:28 +0530

    Add initial compiler design classwork

---
+++ week_2/exercise_1.py
@@ -0,0 +1,14 @@
+age = 22
+gpa = 3.25
+print("\nbefore changing values")
+print("Age : ", age)
+print("Gpa : ", gpa)
+
+age = 25
+gpa = 3.01
+
+print("\nafter changing values")
+print("Age : ", age)
+print("Gpa : ", gpa)
+
+
---
+++ week_2/exercise_2.py
@@ -0,0 +1,5 @@
+x,y,z = 10,20,30
+print(x,y,z)
+
+a=b=c=100
+print(a,b,c)
