d04d52700f6ef200d35eae9cd8acb2ce0f5f49fb
nipunagwdev-maker <nipuna.gw.dev@gmail.com>
2026-09-02 09:37:23 +0530

    updating...

---
+++ week_2/exercise_3.py
@@ -0,0 +1,8 @@
+first_name = "Nipuna"
+last_name = "Madhuranga"
+age = 24
+print(first_name)
+print(last_name)
+
+print("\n",first_name,last_name)
+print(first_name,last_name +" " +str(age))
---
+++ week_2/exercise_4.py
@@ -0,0 +1,9 @@
+#make a simple varibale
+course = "CMIS 3134"
+
+#define a new function to print it
+def show_course():
+    print(course)
+
+#call the function you have made
+show_course
